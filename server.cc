4c4415a2f7f8ce48eece010cf7e3936cf5090349
yoavweber <yoav.weber@code.berlin>
2020-10-03 21:29:18 +0200

    finished following tutorial WIP

--- server.cc
+++ server.cc
@@ -12,11 +12,96 @@
 #include <iomanip>
 #include <thread>
 #include <vector>
+#include <queue>
+#include <condition_variable>
 
 using namespace std;
 
 #define PORT 8080
 #define THREAD_POOL_SIZE 20
+
+//https://stackoverflow.com/questions/121162/what-does-the-explicit-keyword-mean
+
+class ThreadPool
+{
+public:
+    // creating an empty function as a wrapper, not sure wh
+    using Task = std::function<void()>;
+    // try to remove the explicit and see whats going on
+    explicit ThreadPool(size_t numThreads)
+    {
+        start(numThreads);
+    }
+    ~ThreadPool()
+    {
+        stop();
+    }
+
+    // adding tasks to the queue
+    void enqueuq(Task task)
+    {
+        // why are we creating another scope?
+        {
+            unique_lock<mutex> lock{eventMutex};
+            // insert element to the queue, not sure why using the move
+            tasks.emplace(move(task));
+        }
+        // notifying one of the threads to wake up and take the job
+        eventVar.notify_one();
+    }
+
+private:
+    vector<thread> threads;
+
+    condition_variable eventVar;
+
+    mutex eventMutex;
+
+    bool stopping = false;
+    // hold the list of task to be executed on the thread pool
+    queue<Task> tasks;
+    void start(size_t numThreads)
+    {
+        for (int i = 0; i < Num_Threads; i++)
+        {
+            threads.emplace_back([=] {
+                while (true)
+                {
+                    Task task;
+                    // we created another scope cause we don't want the mutex to be lock while the task is executing(11:30)
+                    {
+                        // what is uniuq lock?
+                        unique_lock<mutex> lock{eventMutex};
+                        // the event varible would wake up if we are stopping or if there is task in the queue
+                        eventVar.wait(lock, [=] { return stopping || !tasks.empty(); });
+                        // if its stoping and there are no more tasks we can get out of the thread
+                        if (stopping && tasks.empty())
+                            break;
+
+                        //again not sure what is the move, but it is related to the using Task. but here we are taking a task from the queue and then poping it
+                        task = move(tasks.front());
+                        tasks.pop();
+                    }
+                    // not sure what is executing here
+                    task();
+                }
+            });
+        }
+    }
+    // tell the program not to throw an exception and if it does just let the program crash
+    void stop() noexcept
+    {
+        {
+            unique_lock<mutex> lock{eventMutex};
+            stopping = true;
+        }
+
+        eventVar.notify_all();
+        for (auto &thread : threads)
+            //stop each thread from executitaion
+            thread.join();
+    }
+};
 
 void checkHttpType(string position, int socket);
 void httpRes(ifstream &file, string res, int socket);
@@ -32,12 +117,6 @@ int main(int argc, char const *argv[])
 {
     auto start = std::chrono::high_resolution_clock::now();
     cout << Num_Threads << endl;
-    // vector<thread> threads;
-
-    // for (int i = 0; i < Num_Threads; i++)
-    // {
-    //     threads.
-    // }
 
     int server_fd, new_socket;
 
@@ -83,9 +162,16 @@ int main(int argc, char const *argv[])
         }
 
         // handleConnection(new_socket);
+        // multithread without pool
+        // thread t(&handleConnection, new_socket);
+        // t.detach();
 
-        thread t(&handleConnection, new_socket);
-        t.detach();
+        ThreadPool pool{4};
+
+        for (auto i = 0; i < 4; i++)
+        {
+            pool.enqueuq(&handleConnection, new_socket)
+        }
     }
     printf("------------------Closing connection-------------------\n");
 
@@ -150,7 +236,6 @@ void httpRes(ifstream &file, string res, int socket)
 
 void handleConnection(int socket)
 {
-    sleep(1);
     long valread;
     char buffer[30000] = {0};
     // not sure what is it used
